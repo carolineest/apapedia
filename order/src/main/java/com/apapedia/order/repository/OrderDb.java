284915ca663f2be4b6e648c74616bb4dd9282eae
Syarifah Nur Amalia <syarifah.nur11@ui.ac.id>
2023-11-29 15:46:10 +0700

    solve order

--- order/src/main/java/com/apapedia/order/repository/OrderDb.java
+++ order/src/main/java/com/apapedia/order/repository/OrderDb.java
@@ -8,6 +8,6 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface OrderDB extends JpaRepository<Order, UUID> {
+public interface OrderDb extends JpaRepository<Order, UUID> {
     List<Order> findByStatus(Integer status);
 }
