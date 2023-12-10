eae3d2aa08b606e87516f56f7b1e509c9264cc91
Syarifah Nur Amalia <syarifah.nur11@ui.ac.id>
2023-12-10 11:00:24 +0700

    sprint 3 icha BE

--- order/src/main/java/com/apapedia/order/service/OrderService.java
+++ order/src/main/java/com/apapedia/order/service/OrderService.java
@@ -3,7 +3,9 @@ package com.apapedia.order.service;
 import com.apapedia.order.model.Order;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface OrderService {
     List<Order> getOrderByStatus(Integer status);
+    List<Order> getOrderByCustId(UUID id);
 }
